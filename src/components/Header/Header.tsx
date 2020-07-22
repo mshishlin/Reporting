c3d8589a278ac3df826734c6fecbc354db7dd5ca
Максим Шишлин <m.shishlin@qpdev.ru>
2020-07-22 12:51:29 +0300

    input focus, test user password, header and footer text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export const Header = (props: HeaderProps) => {
     return (
         <div className={classes.header}>
-            <span>Quality Point Development (Тестовое задание по заполнению отчета, Максим Шишлин)</span>
+            <span>QPD (Тестовое задание по заполнению отчета, Максим Шишлин)</span>
             <div className={classes.exit} onClick={props.signOut}>
                 Выход из системы
             </div>
